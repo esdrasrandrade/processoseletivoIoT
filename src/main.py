9d9144680e6c7b32c0f6eb73880f6527bc05ffed
Esdras Rodrigues <esdraskmbt@gmail.com>
2026-04-23 23:44:29 -0300

    update: main.py states + time

--- src/main.py
+++ src/main.py
@@ -14,3 +14,22 @@ button = Pin(BTN_PIN, Pin.IN, Pin.PULL_UP)
 
 joy_x = ADC(Pin(JOY_X_PIN))
 joy_x.atten(ADC.ATTN_11DB)  # faixa 0-3.3V
+
+# ====== ESTADOS ======
+SELECIONANDO = 0
+CONFIRMADO = 1
+
+estado = SELECIONANDO
+indice = 0
+
+# ====== CONTROLE DE TEMPO ======
+ultimo_mov = 0
+ultimo_btn = 0
+
+# ====== FUNÇÕES ======
+def atualizar_leds():
+    for i, led in enumerate(leds):
+        led.value(1 if i == indice else 0)
+
+def ler_joystick():
+    return joy_x.read()
